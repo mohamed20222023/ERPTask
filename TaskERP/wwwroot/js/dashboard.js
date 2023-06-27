0087b91f6412c2289223098687b349543e731ca8
Mohamed Eid <71554599+mohamed20222023@users.noreply.github.com>
2023-06-28 01:21:41 +0300

    Add Charts

--- TaskERP/wwwroot/js/dashboard.js
+++ TaskERP/wwwroot/js/dashboard.js
@@ -1,28 +1,76 @@
 ﻿
 
-
 $.get({
-
 	url: '/Chart/Chart',
 	success: function (data) {
-		console.log(data)
-		var options = {
-			chart: {
-				type: 'area'
+		console.log(data.map(i => i.label))
+		const ctx = document.getElementById('myChart');
+		new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: data.map(i => i.label),
+				datasets: [{
+					label: '# of Votes',
+					data: [20, 30, 40, 50, 60, 70],
+					borderWidth: 1
+				}]
 			},
-			series: [{
-				name: 'Employee',
-				data: /*data.map(i => i.Value)*/[10,20,30,10,70,70]
-			}],
-			xaxis: {
-				categories:/* data.map(i => i.Label)*/ [0,20,40,60,80,100,]
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
 			}
+		});
+
+	}
+})
+
+
+$.get({
+	url: '/Chart/Chart',
+	success: function (data) {
+		console.log(data.map(i => i.label))
+		const ctx = document.getElementById('alaaChart');
+		new Chart(ctx, {
+			type: 'pie',
+			data: {
+				labels: data.map(i => i.label),
+				datasets: [{
+					label: 'My First Dataset',
+					data: [300, 50, 100],
+					backgroundColor: [
+						'rgb(255, 99, 132)',
+						'rgb(54, 162, 235)',
+						'rgb(255, 205, 86)'
+					],
+					hoverOffset: 4
+				}]
+			}
+		});
+
+	}
+})
+
+
+
+function generateExcel() {
+	var table = document.getElementById("example");
+	var data = [];
+
+	for (var i = 0; i < table.rows.length; i++) {
+		var row = [];
+
+		for (var j = 0; j < table.rows[i].cells.length - 1; j++) { // Exclude the last cell
+			row.push(table.rows[i].cells[j].textContent);
 		}
 
-		var chart = new ApexCharts(document.querySelector("#chart"), options);
-
-
-		chart.render();
+		data.push(row);
 	}
-	})
 
+	var wb = XLSX.utils.book_new();
+	var ws = XLSX.utils.aoa_to_sheet(data);
+	XLSX.utils.book_append_sheet(wb, ws, "Dashboard Data");
+	XLSX.writeFile(wb, "dashboard.xlsx");
+}
